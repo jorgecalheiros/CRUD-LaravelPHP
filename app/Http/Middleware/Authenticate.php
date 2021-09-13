e54affd5ae0b12dea0afd19901bcdceb96a369b7
Jorge Calheiros <jorgecalheiros.s@gmail.com>
2021-09-13 18:32:31 -0300

    Correção de erros de rotas -> Modifiquei o nome da rota users.login para auth.login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
-            return route('users.login');
+            return route('auth.login');
         }
     }
 }
