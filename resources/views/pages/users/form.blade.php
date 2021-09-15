6e55af31791835e687783a17f0c6d42f2cb478f7
Jorge Calheiros <jorgecalheiros.s@gmail.com>
2021-09-15 11:50:49 -0300

    Reutilização de trexo HTML -> Reaproveitei o mesmo form do arquivo form.blade para poder mostrar os inputs de editar e de create

--- resources/views/pages/users/form.blade.php
+++ resources/views/pages/users/form.blade.php
@@ -24,45 +24,42 @@
                 Login
             </a>
         </div>
-        <form action="{{ $route }} " class="side-form" method="{{ $method }}">
-            @csrf
-            <x-form.input type="text" name="name" icon="fa-user" placeholder="Insira o seu nome" />
-
-            <x-form.input type="email" name="email" icon="fa-envelope-open-text" placeholder="Digite seu email" />
-
-            <x-form.input type="password" name="password" icon="fa-key" placeholder="Digite a sua senha" />
-
-            <label for="" class="label-input">
-                <i class="fas fa-key icons"></i></i><input type="password" name="reppassword" id="reppassword" autocomplete="off" placeholder="Repita a senha">
-            </label>
-            <button class="btn submit">
-                Criar conta
-            </button>
-        </form>
     @else
-        <form action="{{ $route }}" class="side-form v2" method="{{ $method }}">
+    <div class="side-login">
+        <h1 class="title">Mini gerenciador de conteudos</h1>
+        <h2 class="title-form">Editar conta</h2>
+        <p class="description">Caso não queira editar informações da sua conta clique no botão abaixo</p>
+        <br>
+        <a href="{{ route('users.show', $user->id) }}" class="btn-a">
+            Voltar
+        </a>
+    </div>
+    @endif
+
+        <form action="{{ $route }}" class="side-form" method="{{ $method }}">
             @csrf
             @if ($_method)
                 @method($_method)
             @endif
-            <x-form.input type="text" name="name" icon="fa-user" placeholder="Insira o seu nome" value="{{ $user->name }}"/>
+            @if (!isset($user))
+                <x-form.input type="text" name="name" icon="fa-user" placeholder="Insira o seu nome"/>
+                <x-form.input type="email" name="email" icon="fa-envelope-open-text" placeholder="Digite seu email"  />
+                <x-form.input type="password" name="password" icon="fa-key" placeholder="Digite a sua senha" />
+            @else
+                <x-form.input type="text" name="name" icon="fa-user" placeholder="Insira o seu nome" value="{{ $user->name }}"/>
+                <x-form.input type="email" name="email" icon="fa-envelope-open-text" placeholder="Digite seu email" value="{{$user->email}}" />
+                <x-form.input type="password" name="password" icon="fa-key" placeholder="Digite a sua senha" />
+            @endif
 
-            <x-form.input type="email" name="email" icon="fa-envelope-open-text" placeholder="Digite seu email" value="{{$user->email}}" />
+            @if (!$onlyEdit)
+            <label for="" class="label-input">
+                <i class="fas fa-key icons"></i></i><input type="password" name="reppassword" id="reppassword" autocomplete="off" placeholder="Repita a senha">
+            </label>
+            @endif
 
-            <x-form.input type="password" name="password" icon="fa-key" placeholder="Digite a sua senha" />
             <button class="btn submit">
-                Editar conta
+                {{ $title }}
             </button>
         </form>
-        <div class="side-login">
-            <h1 class="title">Mini gerenciador de conteudos</h1>
-            <h2 class="title-form">Editar conta</h2>
-            <p class="description">Caso não queira editar informações da sua conta clique no botão abaixo</p>
-            <br>
-            <a href="{{ route('users.show', $user->id) }}" class="btn-a">
-                Voltar
-            </a>
-        </div>
-    @endif
  </div>
 @endsection
