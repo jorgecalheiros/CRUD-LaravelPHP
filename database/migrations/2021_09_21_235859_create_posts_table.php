889cc29e58206e91c839dd22e5ef78d727e71a89
Jorge Calheiros <jorgecalheiros.s@gmail.com>
2021-09-22 12:46:48 -0300

    Post -> add PostController -> add Model/Post -> add PostFactory -> add tabela de posts -> add PostSeeder

---
+++ database/migrations/2021_09_21_235859_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("user_id")->unsigned();
+            $table->string("title")->nullable(false);
+            $table->text("content")->nullable(false);
+            $table->enum("status", ["published", "draft"]);
+            $table->softDeletes();
+            $table->timestamps();
+
+            // Relationships
+            $table->foreign("user_id")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeed::class
+            UserSeed::class,
+            PostSeeder::class
         ]);
     }
 }
