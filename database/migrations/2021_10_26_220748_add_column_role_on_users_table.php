31723e1fa7fb656058309852a81ef8c586908212
jorge <jorgecalheiros.s@gmail.com>
2021-11-04 22:05:56 -0300

    Corrindo a migration add_column_role colondo um valor dafault na coluna role

--- database/migrations/2021_10_26_220748_add_column_role_on_users_table.php
+++ database/migrations/2021_10_26_220748_add_column_role_on_users_table.php
@@ -14,7 +14,7 @@ class AddColumnRoleOnUsersTable extends Migration
     public function up()
     {
         Schema::table("users", function (Blueprint $table) {
-            $table->integer("role");
+            $table->integer("role")->default(0);
         });
     }
 
